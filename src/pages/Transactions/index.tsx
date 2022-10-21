9085b389fa86ad4e69568bfb78d3400544ef8478
Drlazinho <lazbonfim@hotmail.com>
2022-10-21 13:50:46 -0300

    commit:  component SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import {
+  PriceHighLight,
+  TransactionContainer,
+  TransactionTable
+} from './styles'
 
 export function Transaction() {
   return (
     <div>
-      <Header/>
-      <Summary/>
+      <Header />
+      <Summary />
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>21/10/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Sanduiche</td>
+              <td>
+                <PriceHighLight variant="outcome">R$ -12,00</PriceHighLight>
+              </td>
+              <td>Compra</td>
+              <td>21/10/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
